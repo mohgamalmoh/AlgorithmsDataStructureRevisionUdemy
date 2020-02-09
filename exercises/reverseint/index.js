281d0cf9cd23c95080ccf49b9a1557ce2e2e0ffc
root <muhammed.gamal@kickstartinteractive.net>
2020-02-09 19:29:16 +0200

    reverse int

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -10,7 +10,28 @@
 
 function reverseInt(n) {
 
+    //let str = n.toString()
+    //let sign = Math.sign(n)
 
+   /* if( sign == -1){
+        str = str.substr(1,str.length)
+    }*/
+    //return  str
+
+   /* let endOfZeroes = str.length
+    for(let i = str.length-1 ; i>=0 ; i--){
+        if(str[i] == 0){
+            endOfZeroes = i
+        }
+
+    }
+    //return endOfZeroes
+    //debugger;
+    str = str.substr(0,endOfZeroes+1)*/
+    let reversed = n.toString().split('').reduce((rev,char)=> char + rev, '')
+    let result =   parseInt(reversed) * Math.sign(n)
+
+    return parseInt(result)
 }
 
 module.exports = reverseInt;
