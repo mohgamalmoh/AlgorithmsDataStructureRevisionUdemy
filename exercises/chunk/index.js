a695b7d7f2d690b1be561f33202565323bf50cb9
root <muhammed.gamal@kickstartinteractive.net>
2020-02-10 13:25:59 +0200

    my own solutions for fizzbuzz + array chunking

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,39 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+
+    let final = []
+
+    for(let i = 0 ; i < array.length ; i += size){
+        final.push(array.slice(i,i+size))
+    }
+
+
+  /*  for(let i = 1 ; i <= array.length ; i ++){
+        chunk.push(array[i-1])
+        if(i % size == 0 || (i == array.length && chunk.length > 0)){
+            final.push(chunk)
+            chunk = []
+        }
+    }*/
+
+    /*for(let i = 1 ; i <= array.length ; i ++){
+        if(i % size == 0 && i <= array.length){
+            chunk.push(array[i-1])
+            final.push(chunk)
+            chunk = []
+
+        }else{
+            chunk.push(array[i-1])
+            if(i == array.length){
+                final.push(chunk)
+
+            }
+        }
+    }*/
+     //console.log(final)
+    return final
+}
 
 module.exports = chunk;
