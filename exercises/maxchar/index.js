746e4ded1e313ce57d440189fcafaa1c586ab9f0
root <muhammed.gamal@kickstartinteractive.net>
2020-02-10 11:58:16 +0200

    maxxhar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,23 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    var obj = {}
+    //let max = 0
+    let maxKey = str[0]
+    /*str.split('').every((char,i)=>{
+        obj[char] = !obj[char] ? 0 : obj[char]++
+    })*/
+
+    for(let char of str){
+        obj[char] = !obj[char] ? 1 : (obj[char]+1)
+        if(obj[char] > obj[maxKey]){
+            maxKey = char
+        }
+
+    }
+
+    return maxKey
+}
 
 module.exports = maxChar;
