434ede04823fb5f50cdb89d01d57a410a8daabbd
root <muhammed.gamal@kickstartinteractive.net>
2020-02-23 11:17:20 +0200

    pyramid printing

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,32 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
 
+    print(n,1,'')
+}
+
+function print(size , row = 1, str){
+
+    //console.log(str.length < (  (((2*size)-1)/2) - (((2*row)-1)/2)  ) || str.length >= (  (((2*size)-1)/2) + (((2*row)-1)/2)  ));
+    if(row > size )
+    {
+        return ;
+    }
+
+    if(str.length == (2*size)-1){
+        console.log(str)
+        return print(size,row+1, '')
+    }
+
+    //str = str.length < row ? str+'#' : str+' '
+
+    if(str.length < (  (((2*size)-1)/2) - (((2*row)-1)/2)  ) || str.length >= (  (((2*size)-1)/2) + (((2*row)-1)/2)  )){
+        str = str + ' '
+    }else{
+        str = str +'#'
+    }
+    print(size,row, str)
+
+}
 module.exports = pyramid;
