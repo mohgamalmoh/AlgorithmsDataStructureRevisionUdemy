bcb54b3928dad3847252027819dec1004532211a
Jimmi <muhammed.gamal@kickstartinteractive.net>
2020-03-04 16:25:59 +0200

    spiral nxn array

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,52 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+
+
+
+
+    let count = 1;
+    let arr = [];
+    for(let f=0 ; f < n ; f ++){
+        arr[f] = []
+        /*for(let r=0 ; r < n ; r ++){
+            arr[f][r] = 'x'
+        }*/
+
+    }
+
+    let startCol = 0
+    let endCol = n
+
+    let startRow = 0
+    let endRow = n
+
+    while (count <= n*n){
+
+        for(var i=startCol ; i < endCol-1 ; i ++){
+            arr[startRow][i] = count++
+        }
+
+        for(var j=startRow ; j < endRow ; j ++){
+            arr[j][i] = count++
+        }
+
+        for(var k=j-2 ; k >= startCol ; k --){ // ok
+            arr[j-1][k] = count++
+        }
+
+        for(var x=j-2 ; x > startRow ; x --){
+            arr[x][startCol] = count++
+        }
+
+        startCol++
+        endCol--
+        startRow++
+        endRow--
+    }
+//console.log(arr)
+    return arr
+}
 
 module.exports = matrix;
