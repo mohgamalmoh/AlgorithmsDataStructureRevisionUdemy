f87e7f0ea0178f9fc463d525fd97612b1b58dae2
root <mohgamalmoh@gmail.com>
2020-03-31 16:04:11 +0200

    linked list part 1

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,79 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+    constructor(data,next = null) {
+        this.data = data;
+        this.next = next;
+    }
+}
 
-class LinkedList {}
+class LinkedList {
+
+    constructor(head = null) {
+        this.head = head
+    }
+
+    insertFirst(data){
+        let newNode = new Node(data,this.head)
+        this.head = newNode
+    }
+
+    size(){
+        let firstNode = this.head;
+        let size = 0;
+        while (firstNode){
+            size++
+            firstNode = firstNode.next
+        }
+        return size
+    }
+
+    getLast(){
+        let firstNode = this.head;
+        let cursor = null
+        while (firstNode ){
+            cursor = firstNode
+            firstNode = firstNode.next
+        }
+        return cursor
+    }
+
+    getFirst(){
+        return this.head
+    }
+
+    clear(){
+        this.head = null
+    }
+
+    removeFirst(){
+        this.head = this.head.next
+    }
+
+    removeLast(){
+        let currentNode = this.head;
+        let nextCursor = null
+        //let nextNextCursor
+        //let precursor = null
+        while (currentNode ){
+            nextCursor = currentNode.next
+
+
+            if(currentNode && nextCursor && !nextCursor.next){
+                currentNode.next = null
+                return
+            }
+
+            if(currentNode && !nextCursor){
+                this.head = null
+                return
+            }
+
+            currentNode = currentNode.next
+        }
+        //return cursor
+    }
+}
 
 module.exports = { Node, LinkedList };
