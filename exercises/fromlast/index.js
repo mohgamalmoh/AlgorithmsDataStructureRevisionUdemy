e6263c9010cf9912695a7822079aa833c237553a
Jimmi <muhammed.gamal@kickstartinteractive.net>
2020-04-02 19:53:01 +0200

    get an element in index n with the reference of last element

--- exercises/fromlast/index.js
+++ exercises/fromlast/index.js
@@ -11,6 +11,20 @@
 //    list.insertLast('d');
 //    fromLast(list, 2).data // 'b'
 
-function fromLast(list, n) {}
+function fromLast(list, n) {
+    fast = list.getFirst();
+    slow = list.getFirst();
+
+    while(n-->0){
+        fast = fast.next
+    }
+
+    while(fast.next){
+        fast = fast.next
+        slow = slow.next
+    }
+
+    return slow
+}
 
 module.exports = fromLast;
