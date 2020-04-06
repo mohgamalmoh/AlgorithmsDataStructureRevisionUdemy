bf36c21586ceed9276149539dfb122934f528270
root <mohgamalmoh@gmail.com>
2020-04-07 00:49:59 +0200

    bst contains

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -37,6 +37,20 @@ class Node {
         }
     }
 
+    contains(data){
+        if(data == this.data){
+            return this
+        }else if(data > this.data && this.right){
+                    return this.right.contains(data)
+        }else if(data < this.data && this.left){
+                    return this.left.contains(data)
+        }else{
+            return null
+        }
+
+
+    }
+
 }
 
 module.exports = Node;
