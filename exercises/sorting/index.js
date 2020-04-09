b552e84a4ac619fa83213281f3a18b091dbe406a
Jimmi <muhammed.gamal@kickstartinteractive.net>
2020-04-09 17:43:03 +0200

    merge sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -2,18 +2,65 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
-
+    for (let i = 0 ; i < arr.length ; i++){
+        for (let j = 0 ; j < arr.length-i-1 ; j++){
+            if(arr[j] > arr[j+1]){
+                let temp = arr[j+1]
+                arr[j+1] = arr[j]
+                arr[j] = temp
+            }
+        }
+    }
+    //console.log(arr)
+    return arr
 }
 
 function selectionSort(arr) {
-
+    for (let i = 0 ; i < arr.length ; i++){
+        let minIdx = i
+        for (let j = i ; j < arr.length ; j++){
+            if(arr[j] < arr[minIdx]){
+                minIdx = j
+            }
+        }
+        if(minIdx != i){
+            let temp = arr[minIdx]
+            arr[minIdx] = arr[i]
+            arr[i] = temp
+        }
+    }
+    //console.log(arr)
+    return arr
 }
 
 function mergeSort(arr) {
+    if(arr.length == 1){
+        return arr
+    }
+    let divider = arr.length/2
+    const left = arr.slice(0,divider)
+    const right = arr.slice(divider,arr.length)
+    let result = merge(mergeSort(left),mergeSort(right))
+    return result;
 
 }
 
 function merge(left, right) {
+    let result = []
+    while(left.length && right.length){
+
+        if(left[0] < right[0]){
+            result.push(left.shift())
+        }else{
+            result.push(right.shift())
+        }
+    }
+
+
+    result.push(...right)
+    result.push(...left)
+
+    return result
 
 }
 
--- exercises/validate/index.js
+++ exercises/validate/index.js
@@ -5,6 +5,20 @@
 // every node's right hand child is greater than
 // the parent
 
-function validate(node, min = null, max = null) {}
+function validate(node, min = null, max = null) {
+
+   if(node.right && !validate(node.right,node.data,max)){
+        return false
+    }else if(node.left && !validate(node.left,min,node.data)){
+        return false
+    }else if(min && node.data < min){
+        return false
+    }else if(max && node.data > max){
+        return false
+    }
+
+    return true
+
+}
 
 module.exports = validate;
